7653acdad4aa1f2b3d74d017f06faae3d0065f11
Daniel Cruz <danielcunha54@gmail.com>
2024-05-06 19:51:08 -0300

    create notification use cases

---
+++ tests/repositories/in-memory-notifications-repository.ts
@@ -0,0 +1,23 @@
+import { NotificationsRepository } from '@/domain/notification/application/repositories/notifications-repository'
+import { Notification } from '@/domain/notification/enterprise/entities/notification'
+
+export class InMemoryNotificationsRepository
+  implements NotificationsRepository
+{
+  public items: Notification[] = []
+
+  async findById(id: string) {
+    return this.items.find((item) => item.id.toString() === id) || null
+  }
+
+  async create(notification: Notification) {
+    this.items.push(notification)
+  }
+
+  async save(notification: Notification) {
+    const idx = this.items.findIndex((item) => item.id === notification.id)
+    if (idx >= 0) {
+      this.items[idx] = notification
+    }
+  }
+}
